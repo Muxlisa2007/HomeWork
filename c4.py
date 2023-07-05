f582333331153a997e84b45eaacc3ab6ae11f45b
Muxlisa2007 <muxlisa200707@gmail.com>
2023-07-05 19:16:55 +0500

    4-masala

---
+++ c4.py
@@ -0,0 +1,7 @@
+sekund = int(input("sutka davomidagi sekundni kiriting: "))
+minut = sekund // 60
+soat = sekund // 3600
+print("hozirgacha bulgan sekund = ",sekund)
+print("hozirgacha bulgan minut = ",minut)
+print("hozirgacha bulgan soat = ",soat)
+
